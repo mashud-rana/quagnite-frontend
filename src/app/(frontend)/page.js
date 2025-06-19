4e01d56744d3e8649ba73a6f0c94aa6e406236b7
rafia0123 <rafiabinterashed01@gmail.com>
2025-06-19 18:03:38 +0600

    school card section completed

--- src/app/(frontend)/page.js
+++ src/app/(frontend)/page.js
@@ -1,9 +1,11 @@
+import About from "@/components/Frontend/Layout/Home/About/About";
 import School from "@/components/Frontend/Layout/Home/School/School";
 
 export default function Home() {
   return (
     <div>
       {/* <h2>home</h2> */}
+      <About />
       <School />
     </div>
   );
