123677fd378a1e13f09efc5f7f0116b81da71781
mdliton <mdliton.itclanbd@gmail.com>
2025-07-14 17:08:09 +0600

    apply-jobs-banner

---
+++ src/app/(frontend)/apply-jobs/page.js
@@ -0,0 +1,10 @@
+import Banner from "@/components/Frontend/ApplyJobs/Banner/Banner";
+import React from "react";
+
+export default function page() {
+  return (
+    <>
+      <Banner />
+    </>
+  );
+}
