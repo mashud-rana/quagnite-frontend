bfdb4072bba8b2596920b1fbf665f99958a1c7b0
mdliton <mdliton.itclanbd@gmail.com>
2025-07-17 15:50:45 +0600

    alumni-details-section

---
+++ src/app/(frontend)/student-hire/page.js
@@ -0,0 +1,10 @@
+import AlumniDetails from "@/components/Frontend/StudentHire/AlumniDetails/AlumniDetails";
+import React from "react";
+
+export default function page() {
+  return (
+    <>
+      <AlumniDetails />
+    </>
+  );
+}
