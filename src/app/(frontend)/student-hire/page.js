bc0f6ace0de5cb4377acc10c71857e31c979e552
mdliton <mdliton.itclanbd@gmail.com>
2025-07-17 16:51:34 +0600

    top-rated-alumni

--- src/app/(frontend)/student-hire/page.js
+++ src/app/(frontend)/student-hire/page.js
@@ -1,10 +1,12 @@
 import AlumniDetails from "@/components/Frontend/StudentHire/AlumniDetails/AlumniDetails";
+import TopRatedAlumni from "@/components/Frontend/StudentHire/TopRatedAlumni/TopRatedAlumni";
 import React from "react";
 
 export default function page() {
   return (
     <>
       <AlumniDetails />
+      <TopRatedAlumni />
     </>
   );
 }
---
+++ src/components/Frontend/StudentHire/TopRatedAlumni/TopRatedAlumni.js
@@ -0,0 +1,61 @@
+"use client";
+import React from "react";
+import styles from "./topRatedAlumni.module.css";
+import Image from "next/image";
+import { Row, Col } from "antd";
+import img1 from "@/assets/images/student-hire/img1.png";
+
+const alumniData = [
+  {
+    name: "Lorem Ipsum Dolar Sit Amet",
+    image: img1,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    name: "Lorem Ipsum Dolar Sit Amet",
+    image: img1,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    name: "Lorem Ipsum Dolar Sit Amet",
+    image: img1,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+];
+
+export default function TopRatedAlumni() {
+  return (
+    <section className="ic_section_space_bottom">
+      <div className="container">
+        <div className={styles.ic_top_rated_main}>
+          <div className={`${styles.ic_top_header} ic_section_heading`}>
+            <h3>Top Rated Alumni</h3>
+            <button type="primary" className="ic-btn1 ic-btn2">
+              See All
+            </button>
+          </div>
+
+          <Row gutter={24}>
+            {alumniData.map((alumni, index) => (
+              <Col xs={24} sm={12} md={12} lg={8} key={index} className="mb-24">
+                <div className={styles.ic_alumni_card}>
+                  <div className={`${styles.ic_alumni_img} mb-12`}>
+                    <Image src={alumni.image} alt={alumni.name} />
+                  </div>
+                  <p className="mb_16">{alumni.name}</p>
+                  <p className="mb-24">{alumni.description}</p>
+                  <button type="default" className="ic-btn1 ic-btn2">
+                    View
+                  </button>
+                </div>
+              </Col>
+            ))}
+          </Row>
+        </div>
+      </div>
+    </section>
+  );
+}
