b365b489d2726d7bb7133c933b0d5e0eb2562055
mdliton <mdliton.itclanbd@gmail.com>
2025-07-17 10:29:01 +0600

    accordion

--- src/app/(frontend)/apply-jobs/page.js
+++ src/app/(frontend)/apply-jobs/page.js
@@ -1,12 +1,14 @@
 import Banner from "@/components/Frontend/ApplyJobs/Banner/Banner";
 import ApplyForJob from "@/components/Frontend/ApplyJobs/ApplyForJob/ApplyForJob";
 import React from "react";
+import Accordion from "@/components/Frontend/ApplyJobs/Accordion/Accordion";
 
 export default function page() {
   return (
     <>
       <Banner />
       <ApplyForJob />
+      <Accordion />
     </>
   );
 }
