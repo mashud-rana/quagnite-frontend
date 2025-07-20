d047f9b9ef420ad432298402f3590a4cb5eb7aa3
mdliton <mdliton.itclanbd@gmail.com>
2025-07-20 16:34:46 +0600

    sponsor-page-animation-add

--- src/components/Frontend/BeASponsor/Sponsor/Sponsor.js
+++ src/components/Frontend/BeASponsor/Sponsor/Sponsor.js
@@ -3,6 +3,7 @@ import styles from "./sponsor.module.css";
 import { Col, Row } from "antd";
 import Image from "next/image";
 import img1 from "@/assets/images/be-a-sponsor/sponsor-left-img.png";
+import CardAnimation from "@/components/Share/ClientComponent/CardAnimation";
 
 const sponsorData = {
   heading: "SPONSOR",
@@ -34,25 +35,29 @@ export default function Sponsor() {
         <div className={styles.ic_sponsor_main}>
           <Row gutter={24} justify="space-between">
             <Col xs={24} md={12} xl={11}>
-              <div className={styles.ic_left}>
-                <div className={`${styles.ic_mainImage} mb_20`}>
-                  <Image src={sponsorData.image} alt="Sponsor" />
+              <CardAnimation index={0} direction="left">
+                <div className={styles.ic_left}>
+                  <div className={`${styles.ic_mainImage} mb_20`}>
+                    <Image src={sponsorData.image} alt="Sponsor" />
+                  </div>
                 </div>
-              </div>
+              </CardAnimation>
             </Col>
             <Col xs={24} md={12} xl={11}>
-              <div className={styles.ic_right}>
-                <h6 className="mb-35">{sponsorData.heading}</h6>
-                <h4 className="mb-24">{sponsorData.title}</h4>
-                {sponsorData.points.map((point, index) => (
-                  <div key={index}>
-                    <h5 className={`${styles.ic_designation} mb_16`}>
-                      {point.subtitle}
-                    </h5>
-                    <p className="ic_section_heading">{point.description}</p>
-                  </div>
-                ))}
-              </div>
+              <CardAnimation index={0} direction="right">
+                <div className={styles.ic_right}>
+                  <h6 className="mb-35">{sponsorData.heading}</h6>
+                  <h4 className="mb-24">{sponsorData.title}</h4>
+                  {sponsorData.points.map((point, index) => (
+                    <div key={index}>
+                      <h5 className={`${styles.ic_designation} mb_16`}>
+                        {point.subtitle}
+                      </h5>
+                      <p className="ic_section_heading">{point.description}</p>
+                    </div>
+                  ))}
+                </div>
+              </CardAnimation>
             </Col>
           </Row>
         </div>
