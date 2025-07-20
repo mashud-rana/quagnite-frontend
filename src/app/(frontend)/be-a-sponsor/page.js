ddbd640ddc2c7128ef4be8a35e75f24db7092c79
mdliton <mdliton.itclanbd@gmail.com>
2025-07-20 14:39:19 +0600

    sponsor

--- src/app/(frontend)/be-a-sponsor/page.js
+++ src/app/(frontend)/be-a-sponsor/page.js
@@ -1,10 +1,12 @@
 import Banner from "@/components/Frontend/BeASponsor/Banner/Banner";
+import Sponsor from "@/components/Frontend/BeASponsor/Sponsor/Sponsor";
 import React from "react";
 
 export default function page() {
   return (
     <>
       <Banner />
+      <Sponsor />
     </>
   );
 }
