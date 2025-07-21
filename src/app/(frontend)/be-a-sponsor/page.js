994002130f011f0658211af3640e04ed6b10c28d
mdliton <mdliton.itclanbd@gmail.com>
2025-07-21 09:34:52 +0600

    banner-component

--- src/app/(frontend)/be-a-sponsor/page.js
+++ src/app/(frontend)/be-a-sponsor/page.js
@@ -1,13 +1,13 @@
-import Banner from "@/components/Frontend/BeASponsor/Banner/Banner";
 import Sponsor from "@/components/Frontend/BeASponsor/Sponsor/Sponsor";
 import Scholarship from "@/components/Frontend/BeASponsor/Scholarship/Scholarship";
 import React from "react";
 import WeOffer from "@/components/Frontend/BeASponsor/WeOffer/WeOffer";
+import SponsorBanner from "@/components/Frontend/BeASponsor/Banner/Banner";
 
 export default function page() {
   return (
     <>
-      <Banner />
+      <SponsorBanner />
       <Sponsor />
       <Scholarship />
       <WeOffer />
---
+++ src/components/Share/Frontend/BannerCard/BannerCard.js
@@ -0,0 +1,35 @@
+"use client";
+import React from "react";
+import styles from "./bannerCard.module.css";
+import { Row, Col } from "antd";
+import Image from "next/image";
+import CardAnimation from "@/components/Share/ClientComponent/CardAnimation";
+
+export default function Banner({ bgImage, rightImage, heading, paragraph }) {
+  return (
+    <section
+      className={styles.ic_banner}
+      style={{ backgroundImage: `url(${bgImage?.src})` }}
+    >
+      <div className="container">
+        <Row gutter={24} align="middle">
+          <Col xs={24} sm={24} md={14} lg={14} xl={12}>
+            <CardAnimation index={0} direction="left">
+              <div className={styles.ic_banner_left}>
+                <h4 className="mb_16">{heading}</h4>
+                <p>{paragraph}</p>
+              </div>
+            </CardAnimation>
+          </Col>
+        </Row>
+      </div>
+      {rightImage && (
+        <div className={styles.ic_banner_right}>
+          <CardAnimation index={0} direction="right">
+            <Image src={rightImage} alt="banner-right" />
+          </CardAnimation>
+        </div>
+      )}
+    </section>
+  );
+}
