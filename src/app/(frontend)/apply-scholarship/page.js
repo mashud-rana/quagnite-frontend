e4277d19bbe0694b73490417265b0dcbbf309164
mdliton <mdliton.itclanbd@gmail.com>
2025-07-21 12:30:20 +0600

    scholarship-section-add

---
+++ src/app/(frontend)/apply-scholarship/page.js
@@ -0,0 +1,10 @@
+import Scholarship from "@/components/Frontend/BeASponsor/Scholarship/Scholarship";
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="ic_section_margin_top_80">
+      <Scholarship />
+    </div>
+  );
+}
