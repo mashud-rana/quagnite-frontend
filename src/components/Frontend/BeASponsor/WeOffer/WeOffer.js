a784fd63fe0446db3f232670778e3930d8fe05fa
mdliton <mdliton.itclanbd@gmail.com>
2025-07-21 09:38:12 +0600

    we-offer-animation-add

--- src/components/Frontend/BeASponsor/WeOffer/WeOffer.js
+++ src/components/Frontend/BeASponsor/WeOffer/WeOffer.js
@@ -4,6 +4,7 @@ import { Col, Row } from "antd";
 import bg from "@/assets/images/be-a-sponsor/we-offer-bg.png";
 import img from "@/assets/images/be-a-sponsor/offer-left.png";
 import Image from "next/image";
+import CardAnimation from "@/components/Share/ClientComponent/CardAnimation";
 
 export default function WeOffer() {
   return (
@@ -15,21 +16,25 @@ export default function WeOffer() {
         <div className={styles.ic_we_offer_main}>
           <Row gutter={24} align="middle" justify="space-between">
             <Col xs={24} md={10}>
-              <div className={styles.ic_we_offer_left}>
-                <Image src={img} alt="img" />
-              </div>
+              <CardAnimation index={0} direction="left">
+                <div className={styles.ic_we_offer_left}>
+                  <Image src={img} alt="img" />
+                </div>
+              </CardAnimation>
             </Col>
             <Col xs={24} md={13}>
-              <div className={styles.ic_we_offer_right}>
-                <h3 className="mb_16">
-                  We offer scholarship to those in need{" "}
-                </h3>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                  dignissim dui id lobortis vulputate. Nulla sodales enim quis
-                  euismod consectetur. 
-                </p>
-              </div>
+              <CardAnimation index={0} direction="right">
+                <div className={styles.ic_we_offer_right}>
+                  <h3 className="mb_16">
+                    We offer scholarship to those in need{" "}
+                  </h3>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Duis dignissim dui id lobortis vulputate. Nulla sodales enim
+                    quis euismod consectetur. 
+                  </p>
+                </div>
+              </CardAnimation>
             </Col>
           </Row>
         </div>
