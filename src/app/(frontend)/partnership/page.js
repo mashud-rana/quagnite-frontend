f2ce7e3920e8f3914f43272a60cf832365c20e75
mdliton <mdliton.itclanbd@gmail.com>
2025-07-27 15:11:20 +0600

    testimomial-card

--- src/app/(frontend)/partnership/page.js
+++ src/app/(frontend)/partnership/page.js
@@ -5,6 +5,33 @@ import img from "@/assets/images/partnership/partner-right.png";
 import ApplyPartner from "@/components/Frontend/Partnership/ApplyPartner/ApplyPartner";
 import Impact from "@/components/Frontend/Partnership/Impact/Impact";
 import Counter from "@/components/Frontend/Partnership/Counter/Counter";
+import Testimonial from "@/components/Share/Frontend/TestimonialCard/TestimonialCard";
+import rating1 from "@/assets/images/apply-scholarship/ratings1.svg";
+import rating2 from "@/assets/images/apply-scholarship/ratings2.svg";
+
+const testimonialData = [
+  {
+    id: 1,
+    name: "Lorem Ipsum Dolar Sit Amet",
+    rating: 5,
+    feedback:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 2,
+    name: "Lorem Ipsum Dolar Sit Amet",
+    rating: 4,
+    feedback:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 3,
+    name: "Lorem Ipsum Dolar Sit Amet",
+    rating: 3,
+    feedback:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+];
 
 export default function page() {
   return (
@@ -18,6 +45,14 @@ export default function page() {
       <ApplyPartner />
       <Impact />
       <Counter />
+      <Testimonial
+        title="TESTIMONIALS"
+        subtitle="Lorem Ipsum Dolar"
+        testimonials={testimonialData}
+        ratingFilledIcon={rating1}
+        ratingEmptyIcon={rating2}
+      />
+      ;
     </>
   );
 }
