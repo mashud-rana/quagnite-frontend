104d05f6872691739a66f45e58db1ba6eaac015b
mdliton <mdliton.itclanbd@gmail.com>
2025-07-27 17:36:55 +0600

    scholarship-video

--- src/app/(frontend)/scholarship/page.js
+++ src/app/(frontend)/scholarship/page.js
@@ -1,10 +1,12 @@
 import Banner from "@/components/Frontend/Scholarship/Banner/Banner";
+import ScholarshipVideo from "@/components/Frontend/Scholarship/ScholarshipVideo/ScholarshipVideo";
 import React from "react";
 
 export default function page() {
   return (
     <>
       <Banner />
+      <ScholarshipVideo />
     </>
   );
 }
