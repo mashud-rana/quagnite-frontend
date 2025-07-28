41092b4f7a0e20af1d10cc6c7c5bc11ac39a0b71
mdliton <mdliton.itclanbd@gmail.com>
2025-07-28 16:06:23 +0600

    related-coach

--- src/app/(frontend)/coach-booking/page.js
+++ src/app/(frontend)/coach-booking/page.js
@@ -1,10 +1,12 @@
 import CoachDetails from "@/components/Frontend/CoachBooking/CoachDetails/CoachDetails";
+import RelatedCoaches from "@/components/Frontend/CoachBooking/RelatedCoaches/RelatedCoaches";
 import React from "react";
 
 export default function page() {
   return (
     <>
       <CoachDetails />
+      <RelatedCoaches />
     </>
   );
 }
