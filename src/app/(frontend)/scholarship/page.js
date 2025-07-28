d938c31db8f683142fae02f99482d0727ff9b3ef
mdliton <mdliton.itclanbd@gmail.com>
2025-07-28 10:09:32 +0600

    success-story

--- src/app/(frontend)/scholarship/page.js
+++ src/app/(frontend)/scholarship/page.js
@@ -1,6 +1,7 @@
 import Banner from "@/components/Frontend/Scholarship/Banner/Banner";
 import ScholarshipVideo from "@/components/Frontend/Scholarship/ScholarshipVideo/ScholarshipVideo";
 import ScholarshipWork from "@/components/Frontend/Scholarship/ScholarshipWork/ScholarshipWork";
+import SuccessStory from "@/components/Frontend/Scholarship/SuccessStory/SuccessStory";
 import React from "react";
 
 export default function page() {
@@ -9,6 +10,7 @@ export default function page() {
       <Banner />
       <ScholarshipVideo />
       <ScholarshipWork />
+      <SuccessStory />
     </>
   );
 }
---
+++ src/components/Frontend/Scholarship/SuccessStory/SuccessStory.js
@@ -0,0 +1,41 @@
+import { Col, Row } from "antd";
+import React from "react";
+import styles from "./successStory.module.css";
+import img from "@/assets/images/scholarship/success.png";
+import Image from "next/image";
+import CardAnimation from "@/components/Share/ClientComponent/CardAnimation";
+
+export default function SuccessStory() {
+  return (
+    <section className={`ic_section_space_bottom ${styles.ic_success}`}>
+      <div className="container">
+        <Row gutter={24}>
+          <Col sm={{ span: 24, order: 2 }} md={{ span: 12, order: 1 }}>
+            <div className={styles.ic_left}>
+              <CardAnimation index={0} direction="up">
+                <h6 className="mb_16">SUCCESS STORIES</h6>
+                <h4 className="mb-35">
+                  Lorem ipsum, consecte adipiscing elit.
+                </h4>
+                <p className="mb-24">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+                  dignissim dui id lobortis vulputate. Nulla sodales enim quis
+                  euismod consectetur.
+                </p>
+                <button className="ic-btn1">explore</button>
+              </CardAnimation>
+            </div>
+          </Col>
+
+          <Col sm={{ span: 24, order: 1 }} md={{ span: 12, order: 2 }}>
+            <div className={styles.ic_right}>
+              <CardAnimation index={0} direction="right">
+                <Image src={img} alt="img" />
+              </CardAnimation>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    </section>
+  );
+}
