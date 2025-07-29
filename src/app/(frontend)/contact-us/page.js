3f2fd9a488f4684df2efd7ad8b8ffe04ae1d7515
mdliton <mdliton.itclanbd@gmail.com>
2025-07-29 10:37:20 +0600

    contact-us-banner

---
+++ src/app/(frontend)/contact-us/page.js
@@ -0,0 +1,17 @@
+import Banner from "@/components/Share/Frontend/BannerCard/BannerCard";
+import React from "react";
+import bg from "@/assets/images/contact-us/banner-bg.png";
+import img from "@/assets/images/contact-us/banner-right.png";
+
+export default function page() {
+  return (
+    <>
+      <Banner
+        bgImage={bg}
+        rightImage={img}
+        heading="Reach out and join the global community of learners."
+        paragraph={`From our dedicated staff and instructor corps to the people who make Quagnite a family, we’re here to answer any questions and help you be the best that you can.`}
+      />
+    </>
+  );
+}
