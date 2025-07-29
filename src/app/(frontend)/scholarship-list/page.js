cf5446f5cacbf9081f4d2566d17ac43cd2100031
rafia0123 <rafiabinterashed01@gmail.com>
2025-07-29 17:43:16 +0600

    updated

---
+++ src/app/(frontend)/scholarship-list/page.js
@@ -0,0 +1,12 @@
+import Products from "@/components/Frontend/Products/Products";
+import React from "react";
+
+const ScholarshipList = () => {
+  return (
+    <div>
+      <Products />
+    </div>
+  );
+};
+
+export default ScholarshipList;
