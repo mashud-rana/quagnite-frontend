ee68d4937e9c7026ce0d15348227453eeece2d22
mamun8893 <mamunurr8893@gmail.com>
2025-08-14 09:57:18 +0600

    student course tab issue fix

--- src/components/Student/Courses/CoursesHeader/CoursesHeader.js
+++ src/components/Student/Courses/CoursesHeader/CoursesHeader.js
@@ -3,10 +3,28 @@
 import { FaArrowLeft } from "react-icons/fa";
 import styles from "./courseHeader.module.css";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import NProgress from "nprogress";
 
 const CoursesHeader = () => {
+  //Router & pathname
   const pathname = usePathname();
+  const router = useRouter();
+  //navigate course
+  const navigateToCourse = (slug) => {
+    NProgress.start();
+    router.push(`/student/${slug}`, {
+      scroll: false,
+    });
+  };
+
+  //nav data
+  const navItems = [
+    { title: "Enrolled courses", slug: "courses" },
+    { title: "Inprogress courses", slug: "courses/inprogress" },
+    { title: "Completed Courses", slug: "courses/completed" },
+  ];
+
   return (
     <div className={styles.ic_header_container}>
       {/* Title Section */}
@@ -16,40 +34,17 @@ const CoursesHeader = () => {
 
       {/* Tabs */}
       <nav className={styles.ic_tabs}>
-        <Link
-          href="/student/courses"
-          className={`${styles.ic_tab_item} ${
-            pathname === "/student/courses" ||
-            (!pathname.startsWith("/student/courses/inprogress") &&
-              !pathname.startsWith("/student/courses/completed"))
-              ? styles.ic_active
-              : ""
-          }`}
-        >
-          Enrolled courses
-        </Link>
-
-        <Link
-          href="/student/courses/inprogress"
-          className={`${styles.ic_tab_item} ${
-            pathname.startsWith("/student/courses/inprogress")
-              ? styles.ic_active
-              : ""
-          }`}
-        >
-          Inprogress pourses
-        </Link>
-
-        <Link
-          href="/student/courses/completed"
-          className={`${styles.ic_tab_item} ${
-            pathname.startsWith("/student/courses/completed")
-              ? styles.ic_active
-              : ""
-          }`}
-        >
-          Completed Courses
-        </Link>
+        {navItems?.map((item) => (
+          <button
+            key={item.slug}
+            className={`${styles.ic_tab_item} ${
+              pathname === `/student/${item.slug}` ? styles.ic_active : ""
+            }`}
+            onClick={() => navigateToCourse(item.slug)}
+          >
+            {item.title}
+          </button>
+        ))}
       </nav>
     </div>
   );
