be40110062e3411105f3fbf2a6a4370eb9ad73ca
rafia0123 <rafiabinterashed01@gmail.com>
2025-08-19 09:55:49 +0600

    space change

--- src/components/Frontend/Products/Products.js
+++ src/components/Frontend/Products/Products.js
@@ -56,7 +56,7 @@ const Products = ({ title }) => {
   ];
 
   return (
-    <section className="ic_section_space">
+    <section className="ic_section_space_top">
       <div className={styles.ic_wrapper}>
         <div className="container">
           <div className={styles.productsSection}>
