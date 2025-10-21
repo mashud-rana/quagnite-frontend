577903efa4e9cb5711dd77a19f1045739a7ee056
Md Nuruzzaman <38957494+nuruzzamanhimel00@users.noreply.github.com>
2025-10-21 18:16:30 +0600

    test web camp and microphone

--- src/app/student/exams/(exams)/page.js
+++ src/app/student/exams/(exams)/page.js
@@ -11,74 +11,12 @@ const Exam = () => {
   const [params, setParams] = useState({
     page: Number(process.env.NEXT_PUBLIC_CURRENT_PAGE) || 1,
     per_page: 9,
+    type: "upcoming",
   });
 
   const [models, setModels] = useState([]);
   const [totalPages, setTotalPages] = useState(1);
   
-  const filteredExams = [
-    {
-      id: "1",
-      title: "Lorem Ipsum dolar asmeit",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis venenatis magna. Aenean mattis id lacus at accumsan. Vestibulum non malesuada e",
-      examId: "8264846",
-      attempts: 3,
-      time: "10:00 AM EST",
-      date: "22 Nov 2024",
-      status: "upcoming",
-      image: "/exam-study-image.png",
-    },
-    {
-      id: "2",
-      title: "Lorem Ipsum dolar asmeit",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis venenatis magna. Aenean mattis id lacus at accumsan. Vestibulum non malesuada e",
-      examId: "8264846",
-      attempts: 3,
-      time: "10:00 AM EST",
-      date: "22 Nov 2024",
-      status: "upcoming",
-      image: "/exam-study-image.png",
-    },
-    {
-      id: "3",
-      title: "Lorem Ipsum dolar asmeit",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis venenatis magna. Aenean mattis id lacus at accumsan. Vestibulum non malesuada e",
-      examId: "8264846",
-      attempts: 3,
-      time: "10:00 AM EST",
-      date: "22 Nov 2024",
-      status: "upcoming",
-      image: "/exam-study-image.png",
-    },
-    {
-      id: "4",
-      title: "Lorem Ipsum dolar asmeit",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis venenatis magna. Aenean mattis id lacus at accumsan. Vestibulum non malesuada e",
-      examId: "8264847",
-      attempts: 2,
-      time: "2:00 PM EST",
-      date: "25 Nov 2024",
-      status: "suggested",
-      image: "/exam-study-image.png",
-    },
-    {
-      id: "5",
-      title: "Lorem Ipsum dolar asmeit",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis venenatis magna. Aenean mattis id lacus at accumsan. Vestibulum non malesuada e",
-      examId: "8264848",
-      attempts: 1,
-      time: "11:00 AM EST",
-      date: "15 Nov 2024",
-      status: "completed",
-      image: "/exam-study-image.png",
-    },
-  ];
-
   
   //fetch api
   const { 
@@ -162,9 +100,7 @@ const Exam = () => {
             </div>
           </InfiniteScroll>
         )}
-      {/* {filteredExams.map((exam) => (
-        <ExamCard key={exam.id} exam={exam} />
-      ))} */}
+    
     </div>
   );
 };
