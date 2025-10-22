9adc563041964f06e18037eb41d272d7bc874a34
mashud-rana <mashud624496@gmail.com>
2025-10-22 15:10:10 +0600

    ..

--- src/app/teacher/(courses)/page.js
+++ src/app/teacher/(courses)/page.js
@@ -10,6 +10,7 @@ import { useEffect, useState } from "react";
 import { Skeleton, Card } from "antd";
 import CourseSkeleton from '@/components/Teacher/Courses/Skeleton/CourseSkeleton'
 import { useRouter } from "next/navigation";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const TeacherDashboardPage = () => {
   const [courses, setCourses] = useState([]);
@@ -51,31 +52,55 @@ const TeacherDashboardPage = () => {
           ) : courses.length === 0 ? (
               <p>No courses found.</p>
           ) : (
-              // <Skeleton active paragraph={{ rows: 4 }} />
-              courses.map((course) => (
-                  <>
+
+              // eslint-disable-next-line react/jsx-key
+              courses.map((course) => (<InfiniteScroll
+                dataLength={courses.length}
+                next={() => {}}
+                hasMore={false}
+                loader={<h4>Loading...</h4>}
+                endMessage={
+                  <p style={{ textAlign: "center" }}>
+                    <b>Yay! You have seen it all</b>
+                  </p>
+                }
+                // below props only if you need pull down functionality
+                refreshFunction={() => {}}
+                pullDownToRefresh
+                pullDownToRefreshThreshold={50}
+                pullDownToRefreshContent={
+                  <h3 style={{ textAlign: "center" }}>&#8595; Pull down to refresh</h3>
+                }
+                releaseToRefreshContent={
+                  <h3 style={{ textAlign: "center" }}>&#8593; Release to refresh</h3>
+                }
+              >
+                {courses.map((course) => (
                   <div key={course.id} className={styles.coachCard}>
                     <div className={styles.coachImage}>
                       <Image
-                          src={course.image || "/placeholder.jpg"}
-                          alt={course.title || "Course"}
-                          width={400}
-                          height={250}
-                          unoptimized
-                          className={styles.coachPhoto}
+                        src={course.image || "/placeholder.jpg"}
+                        alt={course.title || "Course"}
+                        width={400}
+                        height={250}
+                        unoptimized
+                        className={styles.coachPhoto}
                       />
                     </div>
                     <div className={styles.coachInfo}>
                       <h3 className={styles.coachName}>{course.title}</h3>
                       <div className={styles.ic_icon_container}>
-                        <BiEditAlt onClick={(e) => router.push(`/teacher/course/${course?.uuid}`)} className={styles.contactIcon} />
+                        <BiEditAlt
+                          onClick={(e) => router.push(`/teacher/course/${course?.uuid}`)}
+                          className={styles.contactIcon}
+                        />
                         <RiDeleteBin6Line className={styles.contactIcon} />
                       </div>
                     </div>
                   </div>
-                  </>
-              ))
-          )}
+                ))}
+              </InfiniteScroll>
+          )))}
         </div>
       </div>
   );
