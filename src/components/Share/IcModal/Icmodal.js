8caa46be59f3c514951d47efcc6102113e92f186
mashud-rana <mashud624496@gmail.com>
2025-10-23 15:25:43 +0600

    merge all

--- src/components/Share/IcModal/Icmodal.js
+++ src/components/Share/IcModal/Icmodal.js
@@ -1,19 +1,19 @@
-import { Modal } from "antd";
-import React from "react";
+import { Modal } from 'antd';
+import React from 'react';
 
 const Icmodal = () => {
-  return (
-    <Modal
-      title="Search Filter"
-      open={open}
-      onOk={onClose}
-      onCancel={onClose}
-      okText="APPLY FILTER"
-      cancelText="BACK"
-    >
-      <h1>Common modal </h1>
-    </Modal>
-  );
+    return (
+      <Modal
+        title="Search Filter"
+        open={open}
+        onOk={onClose}
+        onCancel={onClose}
+        okText="APPLY FILTER"
+        cancelText="BACK"
+      >
+        <h1>Common modal </h1>
+      </Modal>
+    );
 };
 
 export default Icmodal;
